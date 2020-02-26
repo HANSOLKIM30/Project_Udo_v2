f0ce95a01869eb5d1afe71c55330e14a63a86e7c
RunDoggyRun <51072405+HANSOLKIM30@users.noreply.github.com>
2020-02-26 17:23:18 +0900

    로그아웃 후 경로 수정
    로그아웃 후 경로 수정

--- Project_Udo_v2/src/main/java/com/wad/udo/member/controller/LogoutController.java
+++ Project_Udo_v2/src/main/java/com/wad/udo/member/controller/LogoutController.java
@@ -1,5 +1,9 @@
 package com.wad.udo.member.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -11,15 +15,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LogoutController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String logout(HttpSession session) {
+	public void logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
 		
 		// 세션 무효화 시킴으로서  로그아웃
 		session.invalidate();
 		
 		// 로그인 페이지로 이동
-		String view = "member/loginForm";
-		
-		return view;
+		try {
+			response.sendRedirect(request.getContextPath() + "/member/login");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
