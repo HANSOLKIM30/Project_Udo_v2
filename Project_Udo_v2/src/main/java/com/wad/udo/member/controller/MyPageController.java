fac894dc207efb85d77ea88c531aef512a8a9d12
RunDoggyRun <51072405+HANSOLKIM30@users.noreply.github.com>
2020-02-26 16:56:33 +0900

    myPage requestMapping 경로 수정
    myPage Controller requestMapping 경로 수정

--- Project_Udo_v2/src/main/java/com/wad/udo/member/controller/MyPageController.java
+++ Project_Udo_v2/src/main/java/com/wad/udo/member/controller/MyPageController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("member/myPage")
 public class MyPageController {
 
-	@RequestMapping(value = "member/myPage", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String myPage(){
 		
 		String view = "member/myPage";
