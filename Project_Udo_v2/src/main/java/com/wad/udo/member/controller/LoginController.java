8a297f9b6d0bddc5a41d2dec2c83422f71f528bc
RunDoggyRun <51072405+HANSOLKIM30@users.noreply.github.com>
2020-02-26 16:00:53 +0900

    필요없는 import 삭제
    필요없는 import 삭제

--- Project_Udo_v2/src/main/java/com/wad/udo/member/controller/LoginController.java
+++ Project_Udo_v2/src/main/java/com/wad/udo/member/controller/LoginController.java
@@ -5,7 +5,6 @@ import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
